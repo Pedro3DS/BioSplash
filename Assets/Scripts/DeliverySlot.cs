ca4c00dcf129cc8a4eeaf18ad6fc6bf3fb3b339a
Henrique S Mendonca <razen908h@gmail.com>
2026-05-29 07:19:23 -0400

    bonus por task

--- Assets/Scripts/DeliverySlot.cs
+++ Assets/Scripts/DeliverySlot.cs
@@ -8,14 +8,15 @@ public class DeliverySlot : MonoBehaviour, IInteractable
     public void ReceiveBox(PlayerMovement player)
     {
        FishBox box = player.grabbedObj.GetComponent<FishBox>();
-       ScoreManager.instance.AddScore(box.scoreValue);
+       
         player.DropObject();
         if (OrderSystem.instance.currentOrders.Contains(box.type))
         {
             for (int i = 0; i < OrderSystem.instance.currentOrders.Count; i++)
             {
                 if (OrderSystem.instance.currentOrders[i] == box.type)
-                { 
+                {
+                    box.scoreValue = Mathf.RoundToInt(box.scoreValue * 1.5f);
                     OrderSystem.instance.currentOrders.RemoveAt(i);
                     Destroy(OrderSystem.instance.objectOrders[i]);
                     OrderSystem.instance.objectOrders.RemoveAt(i);
@@ -27,6 +28,7 @@ public class DeliverySlot : MonoBehaviour, IInteractable
                 }
             }
         }
+        ScoreManager.instance.AddScore(box.scoreValue);
         Destroy(box.gameObject);
     }
     public void Interact(PlayerMovement player) => ReceiveBox(player);
